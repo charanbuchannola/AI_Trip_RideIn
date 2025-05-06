78d801b77abb483cfc8612a179e44e9fefb27d5f
charanbuchannola <buchannolacharan@gmail.com>
2025-05-06 20:43:02 +0530

    fix: update MongoDB connection string in db.js

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect(process.env.MONGO_URL)
+    .connect(
+      "mongodb+srv://buchannolacharan:ghpsNKrS7eVnXUV8@cluster0.olj9y.mongodb.net/Ai_trip_RideIn"
+    )
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.error("Could not connect to MongoDB:", err));
 };
